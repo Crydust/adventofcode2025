82ca6d33395923d224bb14168e07b711fabb9839
Kristof Neirynck <kristof.neirynck@iqgeo.com>
2025-12-04 23:03:33 +0100

    Day4Part2 done

---
+++ day04/src/main/java/spike/Day4Part1.java
@@ -0,0 +1,92 @@
+package spike;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.BitSet;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
+
+public class Day4Part1 {
+    static void main(String[] args) throws IOException {
+//        List<String> lines = readInputLines("/example.txt");
+        List<String> lines = readInputLines("/input.txt");
+        int rows = lines.size();
+        int columns = lines.getFirst().length();
+
+        Grid grid = new Grid(rows, columns);
+        for (int y = 0; y < rows; y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < columns; x++) {
+                if (line.charAt(x) == '@') {
+                    grid.fill(x, y);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                if (grid.isRoll(x, y) && grid.countNeighbors(x, y) < 4) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("*** count = " + count);
+    }
+
+    private static final class Grid {
+        private final int rows;
+        private final int columns;
+        private final BitSet items;
+
+        Grid(int rows, int columns) {
+            this.rows = rows;
+            this.columns = columns;
+            items = new BitSet(rows * columns);
+        }
+
+        void fill(int x, int y) {
+            items.set(index(x, y));
+        }
+
+        boolean isRoll(int x, int y) {
+            return items.get(index(x, y));
+        }
+
+        int countNeighbors(int x, int y) {
+            return (int) IntStream.of(
+                            //top
+                            index(x - 1, y - 1),
+                            index(x, y - 1),
+                            index(x + 1, y - 1),
+                            // middle
+                            index(x - 1, y),
+                            index(x + 1, y),
+                            // bottom
+                            index(x - 1, y + 1),
+                            index(x, y + 1),
+                            index(x + 1, y + 1))
+                    .filter(it -> it != -1)
+                    .filter(items::get)
+                    .count();
+        }
+
+        private int index(int x, int y) {
+            if (x < 0 || x >= rows || y < 0 || y >= columns) {
+                return -1;
+            }
+            return (y * columns) + x;
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static List<String> readInputLines(String name) throws IOException {
+        try (var in = new BufferedReader(new InputStreamReader(requireNonNull(Day4Part1.class.getResourceAsStream(name)), UTF_8))) {
+            return in.lines().toList();
+        }
+    }
+}
